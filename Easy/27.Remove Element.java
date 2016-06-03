6413570961ca1b565655d62eedbc8982c343f11e
Wenxiao Song <song.we@husky.neu.edu>
2016-06-02 21:35:17 -0400

    27

---
+++ Easy/27.Remove Element.java
@@ -0,0 +1,13 @@
+public class Solution {
+    public int removeElement(int[] nums, int val) {
+        int cur = 0;
+        for(int i = 0;i<nums.length;i++){
+            if(nums[i] == val){
+                continue;
+            }else{
+                nums[cur++] = nums[i];
+            }
+        }
+        return cur;
+    }
+}
