57a058cb56e163e4534b5a872d1389be656a64b7
Wenxiao Song <song.we@husky.neu.edu>
2016-06-02 21:14:26 -0400

    198

---
+++ Easy/198.House Robber.java
@@ -0,0 +1,13 @@
+public class Solution {
+    public int rob(int[] nums) {
+        if(nums==null||nums.length==0)return 0;
+        int[][] res = new int[nums.length][2];
+        res[0][0] = 0;
+        res[0][1] = nums[0];
+        for(int i = 1;i<nums.length;i++){
+            res[i][0] = Math.max(res[i-1][0],res[i-1][1]);
+            res[i][1] = Math.max(res[i-1][0] + nums[i],res[i-1][1]);
+        }
+        return Math.max(res[nums.length-1][0],res[nums.length-1][1]);
+    }
+}
