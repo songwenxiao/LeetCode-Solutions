295d8aa68642b74175472f04a0a6d421625474db
Wenxiao Song <song.we@husky.neu.edu>
2016-06-06 20:31:41 -0400

    58

---
+++ Easy/58.Length of Last Word.java
@@ -0,0 +1,14 @@
+public class Solution {
+    public int lengthOfLastWord(String s) {
+        s = s.trim();
+        int count = 0;
+        for(int i = s.length()-1;i>=0;i--){
+            if(s.charAt(i)==' '){
+                return count;
+            }else {
+                count++;
+            }
+        }
+        return count;
+    }
+}
