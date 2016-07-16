47e129d12ed53d086e621453f78cf7c4dd66c984
Wenxiao Song <song.we@husky.neu.edu>
2016-07-16 11:46:56 -0400

    69

---
+++ Medium/69.Sqrt(x).java
@@ -0,0 +1,16 @@
+public class Solution {
+    public int mySqrt(int x) {
+        long left = 0, right = x;
+        while(left<=right){
+            long mid = (left+right)/2;
+            if(mid*mid==x){
+                return (int)mid;
+            }else if(mid*mid>x){
+                right = mid-1;
+            }else {
+                left = mid+1;
+            }
+        }
+        return (int)right;
+    }
+}
