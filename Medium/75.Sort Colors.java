0aba7c9ae2d1785687cc0bbe91266c9650f808f5
Wenxiao Song <song.we@husky.neu.edu>
2016-07-30 14:16:55 -0400

    75

---
+++ Medium/75.Sort Colors.java
@@ -0,0 +1,23 @@
+public class Solution {
+    public void sortColors(int[] nums) {
+        if(nums==null||nums.length==0){
+            return;
+        }
+        int idx0 = -1;
+        int idx1 = -1;
+        int idx2 = -1;
+        for(int i = 0;i<nums.length;i++){
+            if(nums[i]==0){
+                nums[++idx2] = 2;
+                nums[++idx1] = 1;
+                nums[++idx0] = 0;
+            }else if(nums[i]==1){
+                nums[++idx2] = 2;
+                nums[++idx1] = 1;
+            }else{
+                nums[++idx2] = 2;
+            }
+        }
+        
+    }
+}
