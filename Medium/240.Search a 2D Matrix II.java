e6a164c02e3f0e766e29c84666008c0d53180939
Wenxiao Song <song.we@husky.neu.edu>
2016-08-02 22:28:01 -0400

    240

---
+++ Medium/240.Search a 2D Matrix II.java
@@ -0,0 +1,19 @@
+public class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        if(matrix == null || matrix.length < 1 || matrix[0].length <1) {
+            return false;
+        }
+        int col = matrix[0].length-1;
+        int row = 0;
+        while(row<=matrix.length-1&&col>=0){
+            if(matrix[row][col]==target){
+                return true;
+            }else if(matrix[row][col]>target){
+                col--;
+            }else{
+                row++;
+            }
+        }
+        return false;
+    }
+}
