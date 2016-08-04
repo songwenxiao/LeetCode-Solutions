aa872a2d4fab6998d4fad6bea942fa1cb3854404
Wenxiao Song <song.we@husky.neu.edu>
2016-08-04 10:49:18 -0400

    162

---
+++ Medium/162.Find Peak Element.java
@@ -0,0 +1,15 @@
+public class Solution {
+    public int findPeakElement(int[] nums) {
+        int left=0,right=nums.length-1;
+        while(left<=right){
+            if(left==right)
+                return left;
+            int mid=(left+right)/2;
+            if(nums[mid]<nums[mid+1])
+                left=mid+1;
+            else
+                right=mid;
+        }
+        return -1;
+    }
+}
