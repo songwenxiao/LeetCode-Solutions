73a27548f4f045cd6e2805def0da98e86b29da56
Wenxiao Song <song.we@husky.neu.edu>
2016-09-10 17:50:24 -0400

    divide and conquer

--- Medium/53.Maximum Subarray.java
+++ Medium/53.Maximum Subarray.java
@@ -13,3 +13,35 @@ public class Solution {
         return max;
     }
 }
+
+//2 divide and conquer
+public class Solution {
+public int maxSubArray(int[] nums) {
+    if(nums == null || nums.length == 0) {
+        return 0;
+    }
+    return dfshelper(nums , 0 , nums.length-1);
+}
+
+protected int dfshelper(int[] nums , int left , int right) {
+    if(left == right) {
+        return nums[left];
+    }
+    int mid = left + (right - left) / 2;
+    int leftmax = dfshelper(nums , left , mid);//   divide part
+    int rightmax = dfshelper(nums , mid+1 , right); //  divide part
+    int lmax = Integer.MIN_VALUE;
+    int rmax = Integer.MIN_VALUE;
+    int leftsum = 0;
+    int rightsum = 0;
+    for(int i = mid ; i >= left ; i--) { //  conquer part
+        leftsum += nums[i];
+        lmax = Math.max(lmax , leftsum);
+    }
+    for(int i = mid+1 ; i <= right ; i++) { //  conquer part
+        rightsum += nums[i];
+        rmax = Math.max(rmax , rightsum);
+    }
+    int crossmax = lmax + rmax; //  conquer part
+    return Math.max(crossmax , Math.max(leftmax , rightmax));   //  combine part
+ }
