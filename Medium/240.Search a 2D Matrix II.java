86c799930ae2138b5a58fcf3a63c304f9d78a031
Wenxiao Song <song.we@husky.neu.edu>
2016-09-10 20:03:15 -0400

    4

--- Medium/240.Search a 2D Matrix II.java
+++ Medium/240.Search a 2D Matrix II.java
@@ -16,4 +16,37 @@ public class Solution {
         }
         return false;
     }
+
+    //2 divide and conquer
+    public boolean searchMatrix(int[][] matrix, int target) {
+    int m = matrix.length;
+    int n = matrix[0].length;
+    if(target < matrix[0][0]) return false;
+    if(target > matrix[m - 1][n - 1]) return false;
+    return searchMatrixHelper(matrix, target, 0, 0);
+}
+
+private static boolean searchMatrixHelper(int[][] matrix, int target, int i,
+        int j) {
+    int m = matrix.length;
+    if(i >= m) return false;
+    int n = matrix[0].length;
+    
+    while(matrix[i][n - 1] < target) i++;
+    return searchMatrixHelper(matrix, target, i + 1, j)||searchInRow(matrix, target, i, j);
+}
+
+private static boolean searchInRow(int[][] matrix, int target, int i, int j) {
+    int n = matrix[0].length;
+    int start = j;
+    int end = n - 1;
+
+    while(start <= end){
+        int mid = (start + end)/2;
+        if(target == matrix[i][mid]) return true;
+        if(target > matrix[i][mid]) start = mid + 1;
+        else end = mid - 1;
+    }
+    return false;
+}
 }
