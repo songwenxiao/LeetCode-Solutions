f0af12cf505cd2b0b1833363ed22de5b828efc3b
Wenxiao Song <song.we@husky.neu.edu>
2016-09-18 12:00:40 -0400

    313

--- Medium/313.Super Ugly Number.java
+++ Medium/313.Super Ugly Number.java
@@ -18,3 +18,38 @@ public class Solution {
     return a[n - 1];
     }
 }
+
+public int nthSuperUglyNumberHeap(int n, int[] primes) {//Heap
+    int[] ugly = new int[n];
+
+    PriorityQueue<Num> pq = new PriorityQueue<>();
+    for (int i = 0; i < primes.length; i++) pq.add(new Num(primes[i], 1, primes[i]));
+    ugly[0] = 1;
+
+    for (int i = 1; i < n; i++) {
+        ugly[i] = pq.peek().val;
+        while (pq.peek().val == ugly[i]) {
+            Num nxt = pq.poll();
+            pq.add(new Num(nxt.p * ugly[nxt.idx], nxt.idx + 1, nxt.p));
+        }
+    }
+
+    return ugly[n - 1];
+}
+
+private class Num implements Comparable<Num> {
+    int val;
+    int idx;
+    int p;
+
+    public Num(int val, int idx, int p) {
+        this.val = val;
+        this.idx = idx;
+        this.p = p;
+    }
+
+    @Override
+    public int compareTo(Num that) {
+        return this.val - that.val;
+    }
+}
