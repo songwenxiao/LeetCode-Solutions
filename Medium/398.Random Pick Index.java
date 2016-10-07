894902a666933681fb7861fa2f1f29aa4bcbc9a4
Wenxiao Song <song.we@husky.neu.edu>
2016-10-07 11:51:18 -0400

    157

---
+++ Medium/398.Random Pick Index.java
@@ -0,0 +1,26 @@
+public class Solution {
+    int[] nums;
+    Random rand;
+    public Solution(int[] nums) {
+        this.nums = nums;
+        this.rand = new Random();
+    }
+    public int pick(int target) {
+        int total = 0;
+        int res = -1;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == target) {
+                // randomly select an int from 0 to the nums of target. If x equals 0, set the res as the current index. The probability is always 1/nums for the latest appeared number. For example, 1 for 1st num, 1/2 for 2nd num, 1/3 for 3nd num (1/2 * 2/3 for each of the first 2 nums).
+                int x = rand.nextInt(++total); 
+                res = x == 0 ? i : res;
+            }
+        }
+        return res;
+    }
+}
+
+/**
+ * Your Solution object will be instantiated and called as such:
+ * Solution obj = new Solution(nums);
+ * int param_1 = obj.pick(target);
+ */
